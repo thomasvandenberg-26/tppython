9f8580a9d07ae9d661dc1c1967c924f2b496dd34
vdbthomas <vandenbergthomas@hotmail.fr>
2024-03-21 19:20:09 +0100

    users

--- ideas.sql
+++ ideas.sql
@@ -1,30 +1,31 @@
+CREATE TABLE USERS(
+     id  INTEGER PRIMARY KEY,
+     Username TEXT,
+     Password TEXT
+     
+);
 CREATE TABLE IDEAS(
      id INTEGER PRIMARY KEY,
     Name TEXT,
     Description TEXT,
     Desire INTEGER,
     Category TEXT,
-    Needed INTEGER
+    Needed INTEGER,
+    user_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES USERS(id) 
 
 );
 
 
-INSERT INTO IDEAS (Name,Description, Desire, Category, Needed) VALUES('Code de la route','Entrainement',3,'Lecon de code', 1);
+INSERT INTO USERS (Username, Password) VALUES ('wadi', 'wadi');
+INSERT INTO USERS (Username, Password) VALUES ('thomas', 'thomas');
 
-INSERT INTO IDEAS (Name,Description, Desire, Category, Needed) VALUES('Jouer de la musique','Musique',5,'Musique', 0);
 
-INSERT INTO IDEAS (Name,Description, Desire, Category, Needed) VALUES('Revision Python','Entrainement dev ',2,'Développement', 1);
 
--- CREATE TABLE USERS(
---      id  INTEGER PRIMARY KEY,
---      Username TEXT,
---      Password TEXT,
---      user_IdeaId INTEGER FOREIGN KEY(user_IdeaId) REFERENCES IDEAS(id),
+INSERT INTO IDEAS (Name,Description, Desire, Category, Needed, user_id) VALUES('Code de la route','Entrainement',3,'Lecon de code', 1,1);
 
-     
+INSERT INTO IDEAS (Name,Description, Desire, Category, Needed , user_id) VALUES('Jouer de la musique','Musique',5,'Musique', 0,2);
 
--- )
+INSERT INTO IDEAS (Name,Description, Desire, Category, Needed, user_id) VALUES('Revision Python','Entrainement dev ',2,'Développement', 0,2);
 
--- INSERT INTO IDEAS (Username, Password ) VALUES ('wadi','wadi');
 
--- INSERT INTO IDEAS (Username, Password ) VALUES ('thomas','thomas');
--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for;
+from flask import Flask, request, redirect, url_for, session;
 from flask import render_template; 
 import sqlite3
 from flask import g
@@ -9,30 +9,39 @@ from pathlib import Path
 DATABASE = 'ideas.db'
 app = Flask(__name__)
 
+app.secret_key ='ok'; 
 ## Routes to get the differentes views of my application
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+## i send the login page
 @app.route("/login")
 def login():
     return render_template('login.html')
 
+## i send the ideas table 
 @app.route("/tableauIdees")
 def tableauIdees():
+    ## i query the database ideas table
     ideas= returnIdeas()
     return render_template('tableauIdees.html',ideas=ideas)
 
+## i call the function to sort the ideas with the number of the desire
 @app.route("/sortedIdeas")
 def sortedIdeas():
+    ## i query the database ideas table to sort the data
     ideas = sortByDesire()
     return render_template('tableauIdees.html',ideas=ideas)
 
         
-@app.route('/formIdea')
-def formIdea():
-    return render_template('formIdeas.html')
+@app.route('/formIdea/<user_id>')
+def formIdea(user_id):
+    if session.get('logged_in'):    
+        return render_template('formIdeas.html', format(user_id))
+    else:
+        return redirect(url_for('login'))
 
 
 ## Method to create a function to post idea into the bdd
@@ -56,11 +65,11 @@ if not Path(DATABASE).exists():
         db.cursor().executescript(sql)
         db.commit()
 
+#method to threat the form id
+@app.route('/submit/<user_id>', methods=['GET'])
 
-@app.route('/submit', methods=['POST'])
-
-def submit():
-    if request.method == 'POST':
+def submit(user_id):
+    if request.method == 'GET':
         name = request.form.get('Name')
         description = request.form.get('Description')
         desire = request.form.get('Desire')
@@ -70,15 +79,15 @@ def submit():
             needed = 1
         elif needed.lower() == "no":
             needed = 0
-    # Initialiser la base de données si nécessaire
-     
-        # Insérer les données dans la table IDEAS
+# Initialiser la base de données si nécessaire
+            
+# Insérer les données dans la table IDEAS
         db = get_db()
 
-        queryInsert = 'INSERT INTO IDEAS (Name, Description, Desire, Category, Needed) VALUES (?, ?, ?, ?, ?);'
+        queryInsert = 'INSERT INTO IDEAS (Name, Description, Desire, Category, Needed WHERE user_id = ?) VALUES (?, ?, ?, ?, ?);'
      
       
-        query_db(queryInsert, [name, description, desire, category, needed] )
+        query_db(queryInsert, [user_id,name, description, desire, category, needed] )
         # Sélectionner et afficher les données insérées
       
         db.commit()
@@ -90,9 +99,10 @@ def query_db(query, args=(), one=False):
     cur.close()
     return (rv[0] if rv else None) if one else rv
 
-def returnIdeas():
-    queryCheckIdeas = 'SELECT Name, Description, Desire, Category, Needed FROM IDEAS;'
-    result = query_db(queryCheckIdeas)
+## return ideas of the user id (username)
+def returnIdeas(id):
+    queryCheckIdeas = 'SELECT Name, Description, Desire, Category, Needed FROM IDEAS Where user_id = ?'
+    result = query_db(queryCheckIdeas, id)
     return result
 
 # i sort table with the desire, i sort with the most important ideas
@@ -100,3 +110,44 @@ def sortByDesire():
     querySort = 'SELECT Name, Description, Desire, Category, Needed FROM IDEAS ORDER BY Desire DESC'
     result = query_db(querySort)
     return result
+
+## connection
+def connection(username, password):
+    query = 'SELECT Username, Password FROM Users WHERE Username = ? and Password = ?;'
+    result = query_db(query, [username, password], one=True)
+    if result:
+        session['logged_in'] = True
+        session['username='] = username
+        
+    else:
+        return "cet utilisateur n'existe pas"
+
+## is the function which get the password and username from the login form 
+@app.route('/connection', methods=['GET','POST'])   
+def processLoginForm():
+    if request.method == 'POST':
+        username= request.form.get('Username')
+        password = request.form.get('Password')
+        if username and password: 
+            connection(username, password)
+            user_id = getIdUser(username)
+        if user_id:
+            return redirect(url_for('formIdea', user_id=user_id ))
+        else:
+            return "user not find"
+
+    else:
+        return redirect(url_for('login'))
+## its the logout  function
+def logout():
+    session.pop('logged_in',None)
+    return redirect(url_for('login'))
+
+def getIdUser(username):
+    query = 'SELECT id FROM USERS where Username = ? '
+    result = query_db(query, [username] )
+    if result:
+        return result[0]['id']
+    else:
+        return None 
+    
